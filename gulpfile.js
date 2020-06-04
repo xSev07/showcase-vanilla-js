ca3216d8f2e42fafdc97a1f35068bc877aca3958
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-06-04 19:50:06 +0500

    Добавляет и настраивает Gulp, меняет структуру проекта для вёрстки

---
+++ gulpfile.js
@@ -0,0 +1,97 @@
+"use strict";
+
+var gulp = require("gulp");
+var del = require("del");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var htmlmin = require("gulp-htmlmin");
+var uglify = require('gulp-uglify');
+var server = require("browser-sync").create();
+
+gulp.task("css", function () {
+  return gulp.src("markup/source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+      "markup/source/fonts/**/*.{woff,woff2}",
+      "markup/source/ico/**"
+    ], {
+      base: "markup"
+    })
+    .pipe(gulp.dest("build"));
+})
+
+gulp.task("images", function () {
+  return gulp.src([
+    "markup/source/img/**/*.{png,jpg,svg}",
+    "!markup/source/img/sprite/**"
+  ])
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("markup/build/img"));
+})
+
+gulp.task("sprite", function () {
+  return gulp.src("markup/source/img/sprite/*.svg")
+    .pipe(svgstore({inlineSvg: true}))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("markup/build/img"));
+})
+
+gulp.task("clean", function () {
+  return del("markup/build");
+})
+
+gulp.task("html", function () {
+  return gulp.src("markup/source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("markup/build"));
+});
+
+gulp.task("build", gulp.series(
+    "clean",
+    "copy",
+    "css",
+    "sprite",
+    "images",
+    "html"
+  ));
+
+gulp.task("server", function () {
+  server.init({
+    server: "markup/build/"
+  });
+
+  gulp.watch("markup/source/sass/**/*.scss", gulp.series("css"));
+  gulp.watch("markup/source/img/sprite/*.svg", gulp.series("sprite", "refresh"));
+  gulp.watch("markup/source/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("start", gulp.series("build", "server"));
